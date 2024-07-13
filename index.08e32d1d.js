37cb9d1349197f7cf4ab230e2f33a5d8c4acc45c
Dexter-24 <67460733+Dexter-24@users.noreply.github.com>
2024-07-13 07:04:26 +0000

    Deploying to gh-pages from @ Dexter-24/team12-proj-js@32b445db2a0683d80f5f5c9663435627a52681eb 🚀

--- index.08e32d1d.js
+++
@@ -1,2 +0,0 @@
-!function(){function t(t){var n={};return new FormData(t.target).forEach((function(t,e){n[e]=t})),n}({form:document.querySelector("#task-form")}).form.addEventListener("submit",(function(n){n.preventDefault();var e=t(n);console.log(e)}))}();
-//# sourceMappingURL=index.08e32d1d.js.map
